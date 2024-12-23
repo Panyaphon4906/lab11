f76ddc3e6a8e61818bcdf9a7cef28d6569cec36b
Ice <panyaphon4906@gmail.com>
2024-12-23 12:28:02 +0700

    complete

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,24 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    
+    cout << "Press Enter 3 times to reveal your future." << endl;
+    
+    int count = 0;
+    while (count < 3) {
+        cin.get();
+        count++;
+    }
+
+    srand(time(0));
+    int randomIndex = rand() % 9;
+
+    cout << "You will get " << grades[randomIndex] << " in this 261102." << endl;
+
+    return 0;
+}
